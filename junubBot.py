68b909b195997626867bcabed3ad9a80501bde66
GeraldManzu <manzugerald@gmail.com>
2022-08-08 03:33:15 -1200

    blocked a user

--- junubBot.py
+++ junubBot.py
@@ -57,7 +57,8 @@ class StreamListener(tweepy.StreamListener):
             'MckaylaMyers123','VetsFlagatGmail','gum_ater','EricaGalvin3','_GenocideDenier','Dylan40405249','I_Find_Species',
             'The_1_one','Raipperi','jal_biel','every98seconds_','BongaPoppy','edward_apet','ExcusesBot','NyanwangkeiM',
             'undoh','AlexMauricioZe2','manufacturer888','K3NYIJHN','mimi12_sadia',
-            'PGHBot','AlemTiop','MahouRoboujo','JustineDhieu','Gatluakofficial']
+            'PGHBot','AlemTiop','MahouRoboujo','JustineDhieu','Gatluakofficial',
+            'Tajcorp211']
             user_tweet = status.user.screen_name
 
             blocked_uza = []
