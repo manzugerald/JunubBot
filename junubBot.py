78a437458a8f2611fa447b99fc812bf863d09ff4
GeraldManzu <manzugerald@gmail.com>
2022-09-08 07:59:38 +0900

    blocked a user

--- junubBot.py
+++ junubBot.py
@@ -46,7 +46,7 @@ class StreamListener(tweepy.StreamListener):
         #         return
 
         if not status.is_quote_status:
-            blocked_users = ['sheikhO12784972','yxvngdvke','cholphela','Gatyangkai20','yahyaah74050732','Salifu14905216','Mutrovic_junior','InsightJuba','EmmsEmms1','permworldcup','happyhealth','blackgayvideo','KenyiKennedy5','BrosMam97','brintonmarcus',
+            blocked_users = ['sheikhO12784972','yxvngdvke','cholphela','Gatyangkai20','yahyaah74050732','Salifu14905216','Mutrovic_junior','InsightJuba','EmmsEmms1','permworldcup','happyhealth','blackgayvideo','KenyiKennedy5','BrosMam97','brintonmarcus','KelDuol',
             'HotXXX88086094', 'mehedimxbd','UgandaIllumin13','kakwaSon','AmosAngong','KakuwaSon','BustaLime91','LuxonSpencer','King_Chris_Jr',
             '80daysbot','WarsGenerator','authenticfabric','dengtielakol', 'githeko','AtWarBot','ConstrainBot',
             'whereistheiss','animal_tech_cop', 'MalongThiel','michael27297382','anaabot','GebregziabherW1','Lokudubenjamin1',
