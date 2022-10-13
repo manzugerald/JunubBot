920bbd07ca3022b4f430bda16dcaef3eabd9c251
GeraldManzu <manzugerald@gmail.com>
2022-10-13 22:27:01 +0900

    adding a user to the blocked list

--- junubBot.py
+++ junubBot.py
@@ -42,7 +42,7 @@ class StreamListener(tweepy.StreamListener):
         #if not status.retw
         # 
         # 
-        # eetedan not tu
+        # eetedan no tu
         # 
         # is_quote_st
         if not status.is_quote_status:
