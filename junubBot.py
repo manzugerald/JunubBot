76747306b378084a7832ab83f7d15f86c2bf30c6
GeraldManzu <manzugerald@gmail.com>
2022-10-18 23:19:59 -1200

    adding a user to the blocked list

--- junubBot.py
+++ junubBot.py
@@ -1,6 +1,6 @@
 ###********** START **********
 ###********** START **********
-###********** STA
+###********** ST
 ###********** Import all the needed **********
 import tweepy #Library for interacting with Twitter
 from tweepy import Stream #For streaming tweets
